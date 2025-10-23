e8849c05c4d134c31771af175e21f5997eb38b0b
smnm1998 <dltpals7498@gmail.com>
2025-10-23 10:28:51 +0900

    fix(validator): 자동차 이름 길이 검증 로직 버그 수정

--- __tests__/validators/CarNameValidator.test.js
+++ __tests__/validators/CarNameValidator.test.js
@@ -11,7 +11,7 @@ describe('CarNameValidator', () => {
   });
 
   describe('validate - 예외 케이스', () => {
-    test.each([[''], [' '], ['   '], ['    ']])(
+    test.each([[''], [' '], ['  '], ['   ']])(
       '빈 문자열이나 공백("%s")은 에러를 발생시킨다.',
       (name) => {
         expect(() => CarNameValidator.validate(name)).toThrow(
@@ -33,20 +33,21 @@ describe('CarNameValidator', () => {
   describe('validate - 여러 이름 검증', () => {
     test('쉼표로 구분된 여러 이름을 검증한다', () => {
       const input = 'pobi,woni,jun';
+      // not을 붙여서 정상 케이스는 에러가 발생하지 않도록 함
       expect(() => CarNameValidator.validateAll(input)).not.toThrow();
     });
 
     test('하나라도 5자를 초과하면 에러를 발생시킨다', () => {
       const input = 'pobi,toolong,jun';
       expect(() => CarNameValidator.validateAll(input)).toThrow(
-        '[ERROR] 이름을 5자 초과한 자동차가 있습니다!',
+        '[ERROR] 자동차 이름은 1자 이상 5자 이하여야 합니다!',
       );
     });
 
     test('빈 문자열이 포함되면 에러를 발생시킨다.', () => {
       const input = 'pobi, ,jun';
-      expect(() => CarNameValidator.validateAll()).toThrow(
-        '[ERROR] 이름이 없는 자동차가 있습니다!',
+      expect(() => CarNameValidator.validateAll(input)).toThrow(
+        '[ERROR] 자동차 이름을 제대로 명시해주세요!',
       );
     });
   });
--- src/validators/CarNameValidator.js
+++ src/validators/CarNameValidator.js
@@ -4,7 +4,7 @@ class CarNameValidator {
       throw new Error('[ERROR] 자동차 이름을 제대로 명시해주세요!');
     }
 
-    if (carName < 1 || carName > 5) {
+    if (carName.length < 1 || carName.length > 5) {
       throw new Error('[ERROR] 자동차 이름은 1자 이상 5자 이하여야 합니다!');
     }
   }

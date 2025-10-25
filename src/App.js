2d4cd3421087d98288352a94b2f3aeed04ba0961
smnm1998 <dltpals7498@gmail.com>
2025-10-25 16:21:13 +0900

    feat(app): 게임 진행 로직 구현
    - 지정된 횟수만큼 라운드 반복
    - 랜덤값 생성 헬퍼 메서드 분리
    - 라운드별 결과 출력

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import RoundCountValidator from './validators/RoundCountValidator.js';
 import RacingGame from './models/RacingGame.js';
 
 class App {
+  #game;
+  #roundCount;
+
   async run() {
     try {
       await this.#setupGame();
@@ -26,10 +29,30 @@ class App {
     // 라운드 횟수 입력 및 검증
     const roundInput = await InputView.readRoundCount();
     RoundCountValidator.validateInput(roundInput);
-    this.roundCount = Number(roundInput.trim());
+    this.#roundCount = Number(roundInput.trim());
 
     // RacingGame 객체 생성
-    this.game = new RacingGame(carNames);
+    this.#game = new RacingGame(carNames);
+  }
+
+  async #playGame() {
+    for (let i = 0; i < this.#roundCount; i++) {
+      const randomValues = this.#generateRandomValues();
+      this.#game.playRound(randomValues);
+      OutputView.printRoundResult(this.#game.getCars());
+    }
+  }
+
+  #announceWinner() {
+    const winners = this.#game.getWinners();
+    OutputView.printWinners(winners);
+  }
+
+  #generateRandomValues() {
+    const carCount = this.#game.getCars().length;
+    return Array.from({ length: carCount }, () =>
+      Random.pickNumberInRange(0, 9),
+    );
   }
 }
 
